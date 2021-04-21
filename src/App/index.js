d8412302e366b35f68b9230f2d2b736b6955f2e9
moisestech <m@moises.tech>
2021-04-21 05:02:46 -0400

    prettier syntax

---
+++ src/App/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import SketchWrapper from '../components/SketchWrapper';
+
+export default function App() {
+  return (
+    <div className="sketch-container">
+      <h1>Polynomial Regression with Tensorflow.js</h1>
+      <SketchWrapper />
+    </div>
+  );
+}
