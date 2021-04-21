0c8d05b6ac199349ac678c31cd3a1dd6dee0de25
moisestech <m@moises.tech>
2021-04-21 05:01:49 -0400

    prettierrc

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
-// https://thecodingtrain.com/CodingChallenges/105-polynomial-regression-tfjs.html
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles.css';
+import App from './App';
 
-// https://www.youtube.com/watch?v=tIXDik5SGsI
-
-const project_name = "polynomial regression";
-
-console.log(project_name);
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
