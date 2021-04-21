44e0fd3af296a8cfeb65e8ae0e85335595deaf51
moisestech <m@moises.tech>
2021-04-21 05:03:15 -0400

    prettier syntax

---
+++ src/p5/p5_polynomial_regress.js
@@ -0,0 +1,8 @@
+// Daniel Shiffman
+// http://codingtra.in
+// http://patreon.com/codingtrain
+
+// Polynomial Regression with TensorFlow.js
+// Video: https://www.youtube.com/watch?v=tIXDik5SGsI
+
+// coming soon
